4a559733974bca34ee6994bf009de5f8129f4654
Lukas Batema <lukasbatema@gmail.com>
2021-04-16 08:38:00 -0400

    CLean up the useless button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function buttonOne() {
+    buttonOneLabel = "Just A Random Button That Does Nothing!";
+    buttonOneJS = window.location.href = 'https://www.youtube.com/watch?v=BjDebmqFRuc&t=6s';
 
-    buttonOneLabel = "Just A Random Button That Does Nothing";
-    buttonOneJS = "onClick=\"window.location.href='https://www.youtube.com/watch?v=BjDebmqFRuc&t=6s';\"";
-    
-    document.getElementById("buttonOne").innerHTML += buttonOneLabel;
-    document.getElementById("buttonOne").outerHTML += buttonOneJS;
+    document.getElementById("buttonOne").innerHTML += 
+      buttonOneLabel;
+    document.getElementById("buttonOne").onclick(buttonOneJS);
 }
