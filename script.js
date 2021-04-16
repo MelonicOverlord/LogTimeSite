e2c1cb359a86e7281b0c00b2c0a1a6a8617c0d39
Lukas Batema <lukasbatema@gmail.com>
2021-04-16 10:54:40 -0400

    Fix button again

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 function buttonOne() {
-    buttonOneLabel = "Just A Random Button That Does Nothing!";
-    buttonOneJS = window.location.href = 'https://www.youtube.com/watch?v=BjDebmqFRuc&t=6s';
-
-    document.getElementById("buttonOne").innerHTML += 
-      buttonOneLabel;
-    document.getElementById("buttonOne").onclick(buttonOneJS);
+  document.getElementById("buttonOne").onclick(window.location.href = 'https://www.youtube.com/watch?v=BjDebmqFRuc&t=6s');
 }
